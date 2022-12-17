592b76ae9a0718084d4e6b353a761a991227a94a
gyeongguk.park <93904398+GGpark1@users.noreply.github.com>
2022-12-18 00:31:33 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,28 @@
+N, K = map(lambda x: int(x), input().split())
+
+coins = []
+
+for _ in range(N):
+    coin = int(input())
+    if K // coin != 0:
+        coins.append(coin)
+
+coins.sort(reverse=True)
+
+count = 0
+
+for coin in coins:
+    divide = K // coin
+    remain = K % coin
+    if K >= coin:
+        if remain == 0:
+            count += divide
+            break
+        else:
+            count += divide
+            K = remain
+    else:
+        continue
+
+
+print(count)
