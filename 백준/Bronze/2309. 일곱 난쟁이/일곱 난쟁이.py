0932ec4757185025b0a504d30a5e6511ea8441bf
gyeongguk.park <93904398+GGpark1@users.noreply.github.com>
2022-12-17 23:02:00 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,18 +1,10 @@
 from itertools import combinations
 
-height_list = []
-
-for _ in range(9):
-    height = int(input())
-    height_list.append(height)
-
+height_list = [int(input()) for _ in range(9)]
 
 for height_combination in combinations(height_list, 7):
     if sum(height_combination) == 100:
-        height_combination = list(height_combination)
+        for height in sorted(height_combination):
+            print(height)
+            
         break
-
-height_combination.sort()
-
-for height in height_combination:
-    print(height)
