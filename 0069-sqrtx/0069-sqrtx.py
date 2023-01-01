9acde88ad50251d4654499130defbf8b1e1d873c
gyeongguk.park <93904398+GGpark1@users.noreply.github.com>
2023-01-02 01:54:03 +0900

    Time: 2729 ms (16.13%), Space: 13.8 MB (55.65%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,11 @@
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        for i in range(1,x+1):
+            if i * i == x:
+                return i
+            elif i * i > x:
+                return (i-1)
